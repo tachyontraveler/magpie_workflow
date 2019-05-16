d00d62e513a9f97484510238342b295039b41829
Abhijith Gopakumar <abhijithmg@gmail.com>
2019-05-16 17:12:18 -0500

    first fully functioning commit

---
+++ Magpie/README.md
@@ -0,0 +1,39 @@
+# Magpie #
+
+[![Coverage Status](https://coveralls.io/repos/wolverton/magpie/badge.svg?branch=master&service=bitbucket)](https://coveralls.io/bitbucket/wolverton/magpie?branch=master) [![Statistics](https://www.openhub.net/p/wolverton_magpie/widgets/project_thin_badge.gif)](https://www.openhub.net/p/wolverton_magpie) 
+
+Magpie, the Materials-Agnostic Platform for Informatics and Exploration, is designed to simplify the use of machine learning to predict properties of materials. 
+
+## How do I get Magpie? ##
+
+There are currently two options for installing Magpie
+
+### Building from Source ###
+
+First, download or clone a copy of this repository from BitBucket. 
+
+```
+#!shell
+git clone https://bitbucket.org/wolverton/magpie
+```
+
+To build with Gradle, use the included Gradle wrapper (which will install Gradle on your system if needed):
+
+```
+#!shell
+
+./gradlew jar
+```
+
+### Downloading a Precompiled Version ###
+
+A complete version of Magpie that has already been compiled is available for **[download](http://oqmd.org/static/analytics/magpie/Magpie.zip)**. Since Magpie is written entirely in Java, this version should work without modification on any system with the Java Runtime Environment installed.
+
+## How do I learn to use Magpie? ##
+
+Documentation for Magpie is available on [oqmd.org](http://oqmd.org/static/analytics/magpie/doc/). You may want to start with the [tutorial](http://oqmd.org/static/analytics/magpie/doc/tutorial.html).
+
+
+## Who can answer my questions? ##
+
+Please contact any of the development team through their BitBucket accounts. If you have found an error, please open up an issue in BitBucket's issue tracker with a detailed explanation of what went wrong.
--- workflow.py
+++ workflow.py
@@ -2,17 +2,18 @@ import subprocess
 import sys 
 import glob 
 import os 
+import shutil
      
 class magpie_workflow:
     def __init__(self):
+        self.outdir='./OUTDIR/'
         self.poscars_dir = './input_poscars/'
         self.poscars_ext = '*'
-        self.prop_dir    = './software/Magpie/lookup-data/' 
-        self.magpie_exec = ['java', '-jar', './software/Magpie/dist/Magpie.jar']
-        self.poscars_dir = './input_poscars/'
-        self.GEN_TAG     = ''
+        self.prop_dir    = './Magpie/lookup-data/'
+        self.magpie_exec = ['java', '-jar', './Magpie/Magpie.jar']
+
     def OWrite(self,s): 
-        with open('./out.workflow.txt','a') as fout: 
+        with open(os.path.join(self.outdir,'out.workflow.txt'),'a') as fout: 
             fout.write(s) 
         print(s) 
         sys.stdout.flush()     
@@ -20,45 +21,48 @@ class magpie_workflow:
     def run_magpie(self): 
         self.runfile_ext = [self.mag_ip_file+'.in','|', 'tee', self.mag_ip_file+'.out'] 
      
-        with open('./out.workflow.txt','a') as fout: 
-            with open('./err.worksflow.txt','a') as ferr: 
-                out=subprocess.call(self.magpie_exec+runfile_ext, stdout=fout, stderr=ferr) 
+        with open(os.path.join(self.outdir,'out.workflow.txt'),'a') as fout: 
+            with open(os.path.join(self.outdir,'err.worksflow.txt'),'a') as ferr: 
+                out=subprocess.call(self.magpie_exec+self.runfile_ext, stdout=fout, stderr=ferr) 
         sys.stdout.flush() 
      
      
     def input_gen(self): 
-        self.mag_feats_file  = './'+self.GEN_TAG+'features_generated_magpie.csv' 
-        magpie_comms = ['data = new data.materials.CrystalStructureDataset', \ 
-                        'timer start', 'data import '+self.poscars_dir, 'timer elapsed', \ 
-                        'data attributes properties directory '+self.prop_dir, \ 
-                        'data attributes properties add set general', \ 
-                        'data target local_reference', \ 
-                        'attr = new utility.tools.BatchAttributeGenerator 1000 DelimitedOutput ,', \ 
-                        'timer start', 'attr write '+self.mag_feats_file+' $data', \ 
+        self.mag_feats_file  = os.path.join(self.outdir,'features_generated_magpie.csv')
+        magpie_comms = ['data = new data.materials.CrystalStructureDataset', \
+                        'timer start', 'data import '+self.poscars_dir, 'timer elapsed', \
+                        'data attributes properties directory '+self.prop_dir, \
+                        'data attributes properties add set general', \
+                        'data target local_reference', \
+                        'attr = new utility.tools.BatchAttributeGenerator 1000 DelimitedOutput ,', \
+                        'timer start', 'attr write '+self.mag_feats_file+' $data', \
                         'timer elapsed', 'exit'] 
      
-        self.mag_ip_file = './'+self.GEN_TAG+'generate-attributes' 
+        self.mag_ip_file = os.path.join(self.outdir,'generate-attributes')
         with open(self.mag_ip_file+'.in','w') as fout: 
             fout.write('\n'.join(magpie_comms)) 
      
-    def reference_gen(self): 
+    def reference_gen(self):
+        if os.path.isfile(self.poscars_dir+'properties.txt'):
+            os.remove(self.poscars_dir+'properties.txt') 
         if not self.poscars_ext.startswith('*'): 
             self.poscars_ext = '*'+self.poscars_ext 
      
         files_list = glob.glob(os.path.join(self.poscars_dir,self.poscars_ext)) 
         files_list = [' '.join([item.split('/')[-1],str(i)]) for i,item in enumerate(files_list)]
-        title      = ' '.join(['filename','local_reference'])
+        title      = [' '.join(['filename','local_reference'])]
         with open(os.path.join(self.poscars_dir+'properties.txt'),'w') as fout:
             fout.write('\n'.join(title+files_list))
 
     def post_process(self):
-        if not os.path.isfile(self.feats_file+'.out'):
+        if not os.path.isfile(self.mag_feats_file):
             print('magpie output file not found. Run Magpie first')
             return
-        data = open(self.mag_feats_file,'r').read().strip().split('\n')
-        data = [item.strip().split(',') for item in data]
+        data  = open(self.mag_feats_file,'r').read().strip().split('\n')
+        data  = [item.strip().split(',') for item in data]
         title = ','.join(['filename']+data[0][:-1])
-        self.processed_feats_file = './'+self.GEN_TAG+'features_processed_final.csv'
+        data  = data[1:] 
+        self.processed_feats_file = os.path.join(self.outdir,'features_processed_final.csv')
 
         ref_data = open(os.path.join(self.poscars_dir+'properties.txt'),'r').read().strip().split('\n')[1:]
         for i,item in enumerate(data):
@@ -71,13 +75,18 @@ class magpie_workflow:
          
      
      
-def main(): 
+def main():
     print('Beginning the workflow')
     workflow = magpie_workflow()
+
+    if os.path.isdir('./OUTDIR'):
+        shutil.rmtree('./OUTDIR')
+    os.mkdir('./OUTDIR')
+
     workflow.reference_gen()
     workflow.input_gen()
     workflow.run_magpie()
     workflow.post_process()
     print('Done')
 
-     
+main()     
